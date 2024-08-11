270574b741320924d3b04616792c6d04782d9a52
Konstantin <Kostion37@yandex.ru>
2024-08-11 11:56:25 +0300

    v1.3

--- backend/api/management/commands/impdata.py
+++ backend/api/management/commands/impdata.py
@@ -14,19 +14,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         n = 0
-        for models, file_name in TABLES.items():
+        for model, file_name in TABLES.items():
+            add = 0
+            sum = 0
+            self.stdout.write(self.style.WARNING(
+                f'Импорт данных из {file_name}')
+            )
             with open(
                 f'{settings.BASE_DIR}/data/{file_name}',
                 'r',
                 encoding='utf-8'
             ) as file:
                 reader = DictReader(file)
-                data = []
                 for line in reader:
-                    data.append(models(**line))
-                models.objects.bulk_create(data)
-            self.stdout.write(self.style.WARNING(
-                f'Импорт данных из {file_name}...')
+                    sum += 1
+                    try:
+                        model.objects.create(**line)
+                        add += 1
+                    except Exception:
+                        continue
+            self.stdout.write(self.style.SUCCESS(
+                f'Импорт из {file_name} закончен. Добавлено {add} из {sum}')
             )
             n += 1
         if n == len(TABLES):
